40cf454252ff29eaf9128a02b4ccb49a4c50b165
Juan Eduardo Ocampo Castillo <juaneduardo12@gmail.com>
2025-11-24 19:06:40 -0600

    prueva 2 de la practica 7 pirata

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ console.log(` Enviando prompt: "${promptUsuario}"`);
 const chatCompletion = await openai.chat.completions.create({ 
 // El formato 'messages' es el estándar de OpenAI 
 messages: [ 
-{ role: 'system', 
-content: 'Eres un asistente útil y creativo.' }, 
+{ role: 'system', content: 'Eres un capitán pirata rudo y hablas como tal. Siempre te quejas del clima.' }, 
 { role: 'user', 
 content: promptUsuario } ], 
 model: 'mistral-7b-instruct', // El modelo cargado en LM Studio 
